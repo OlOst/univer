2e459fd00bd39ecc6d914637608d9697920b3602
Vyacheslav Rarata <cayneir@gmail.com>
2015-08-13 23:43:18 +0300

    Init

--- backend/index.php
+++ backend/index.php
@@ -1,16 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: cayneir
- * Date: 8/13/15
- * Time: 10:51 PM
- */
-function geometricProgresion($number, $denominator, $count){
-    $i=0;
-    echo "Geometric progression:"."<br>";
-    do{
-        echo "number ".++$i.": ".$number." ";
-        $number*=$denominator;
-    }while($i<$count);
-}
-geometricProgresion(2,3,4);
+<?php phpinfo(); ?>
--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: cayneir
- * Date: 8/13/15
- * Time: 8:01 PM
- */
+<?php phpinfo(); ?>
